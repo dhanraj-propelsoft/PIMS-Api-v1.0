0216f1ed02f2091c566cc8ef742e558bb1947be3
kajith <kajithaji98@gmail.com>
2023-10-09 12:12:47 +0530

    Temp Org Seperated Data

--- app/Http/Services/Api/OrganizationMaster/OrganizationService.php
+++ app/Http/Services/Api/OrganizationMaster/OrganizationService.php
@@ -20,7 +20,60 @@ class OrganizationService
             $orgDetails = json_decode($tempOrgItem->organization_detail, true);
             $orgDocuments = json_decode($tempOrgItem->organization_doc_type, true);
             $orgAddress = json_decode($tempOrgItem->organization_address, true);
-            return ['orgDetails' => $orgDetails, 'orgDocuments' => $orgDocuments, 'orgAddress' => $orgAddress];
+            $orgName = $orgDetails['orgName'];
+            $orgEmail = $orgDetails['orgEmail'];
+            $orgwebsite = $orgDetails['orgwebsite'];
+            $orgStructureId = $orgDetails['orgStructureId'];
+            $orgCategoryId = $orgDetails['orgCategoryId'];
+            $orgOwnershipId = $orgDetails['orgOwnershipId'];
+            $doorNo = $orgAddress['doorNo'];
+            $buildingName = $orgAddress['buildingName'];
+            $street = $orgAddress['street'];
+            $landMark = $orgAddress['landMark'];
+            $pinCode = $orgAddress['pinCode'];
+            $districtId = $orgAddress['districtId'];
+            $stateId = $orgAddress['stateId'];
+            $cityId = $orgAddress['CityId'];
+            $area = $orgAddress['area'];
+            $location = $orgAddress['location'];
+
+            $mappedDocuments = [];
+            if ($orgDocuments) {
+                foreach ($orgDocuments as $document) {
+                    $docTypeId = $document['doctypeId'];
+                    $docNo = $document['docNo'];
+                    $docValid = $document['docValid'];
+                    $docFilePath = $document['docFilePath'];
+
+                    $mappedDocument = [
+                        'docTypeId' => $docTypeId,
+                        'docNo' => $docNo,
+                        'docValid' => $docValid,
+                        'docFilePath' => $docFilePath,
+                    ];
+
+                    $mappedDocuments[] = $mappedDocument;
+                }
+            }
+            return [
+                'orgName' => $orgName,
+                'orgEmail' => $orgEmail,
+                'orgwebsite' => $orgwebsite,
+                'orgStructureId' => $orgStructureId,
+                'orgCategoryId' => $orgCategoryId,
+                'orgOwnershipId' => $orgOwnershipId,
+                'orgDocuments' => $mappedDocuments,
+                'doorNo' => $doorNo,
+                'buildingName' => $buildingName,
+                'street' => $street,
+                'landMark' => $landMark,
+                'pinCode' => $pinCode,
+                'districtId' => $districtId,
+                'stateId' => $stateId,
+                'CityId' => $cityId,
+                'area' => $area,
+                'location' => $location,
+            ];
         });
         Log::info('OrganizationService >Store Return.' . json_encode($tempOrganization));
         return new SuccessApiResponse($tempOrganization, 200);
